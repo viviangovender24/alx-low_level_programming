676ea8ea49c153077ad08b36e8e924bf94c85eaa
viviangovender24 <vivian.govender@live.com>
2023-02-16 07:52:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - using puts function to print a line
+*Return: 0 (complete)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
