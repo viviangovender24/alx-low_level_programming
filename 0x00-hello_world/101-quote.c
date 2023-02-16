4568438610236c45cab1a08a86d9a573d0e1b09b
viviangovender24 <vivian.govender@live.com>
2023-02-16 10:19:53 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point, print a line
+ * Return:1 (1 if program succesful)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
