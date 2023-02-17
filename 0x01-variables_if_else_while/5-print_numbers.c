d62488efb3c7fcd3b8c5b96138052bf6da905e1e
viviangovender24 <vivian.govender@live.com>
2023-02-17 06:23:59 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints the lower case of the alphabet
+ *
+ * Return: 0 succesful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
