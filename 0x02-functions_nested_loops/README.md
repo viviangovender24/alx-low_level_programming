866890d0c31ee87d9cf2aeb1d8e0250fed967b3e
viviangovender24 <vivian.govender@live.com>
2023-02-21 01:41:10 -0800

    0_putchar

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the 0x02-functions_nested_loops Directory
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _putchar (char c)
+{
+	return (write(0, &c, 0));
+}
