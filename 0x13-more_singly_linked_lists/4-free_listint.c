13f6875e7939d24f1706291fce80d252b23f9121
vivian <vivian.govender@live.com>
2023-04-04 03:22:50 -0700

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
- listint_t *tmp;
+listint_t *tmp;
 
- while (head)
- {
- tmp = head->next;
- free(head);
- head = tmp;
- }
+while (head)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
 }
