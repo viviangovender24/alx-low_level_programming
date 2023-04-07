7b34532c08dc4676fbeb5d0e5ae84730ce5bf4ba
vivian <vivian.govender@live.com>
2023-04-07 06:55:37 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* clear_bit - set value of a bit to 0
+* @n: pointer to number to change
+* @index: index of bit to clear
+* Return: 1 for complete, -1 for uncomplete
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+*n = (~(1UL << index) & *n);
+return (1);
+}
