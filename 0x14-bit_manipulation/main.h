029ad0c61045dac91959204b49eb617d994b49d6
vivian <vivian.govender@live.com>
2023-04-07 01:02:09 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,8 @@
 
 int _putchar(char cha);
 unsigned int binary_to_uint(const char *b);
-/*void print_binary(unsigned long int n);
+void print_binary(unsigned long int n);
+/*
 /int get_bit(unsigned long int n, unsigned int index);
 /int set_bit(unsigned long int *n, unsigned int index);
 /int clear_bit(unsigned long int *n, unsigned int index);
