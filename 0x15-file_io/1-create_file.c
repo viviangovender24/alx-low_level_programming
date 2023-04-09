854e1b84fd943399608e19c677d1f73365086846
vivian <vivian.govender@live.com>
2023-04-09 01:22:31 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* create_file - Creates a file.
-* @filename: A pointer to the name of the file to create.
-* @text_content: A pointer to a string to write to the file.
+* create_file - Creates file.
+* @filename: name of the file to create.
+* @text_content:string to write to the file.
 *
 * Return: If the function fails - -1.
 * Otherwise - 1.
